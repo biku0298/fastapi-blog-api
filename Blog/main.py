ff7451b2f6711c0235a195b9a4ee7822a3c2c793
biku0298 <77164059+biku0298@users.noreply.github.com>
2026-02-20 22:10:09 +0530

    Update main.py

--- Blog/main.py
+++ Blog/main.py
@@ -3,10 +3,16 @@ from . import model
 from fastapi import FastAPI
 from .database import engine
 from .routers import blog,user,authentication
-
+from fastapi.responses import HTMLResponse
 app = FastAPI()
 
-
+@app.get("/", response_class=HTMLResponse)
+def home():
+    return """
+    <h1>FastAPI Blog API</h1>
+    <p>API is running successfully 🚀</p>
+    <a href="/docs">Go to API Docs</a>
+    """
 
 model.Base.metadata.create_all(bind=engine)
 
